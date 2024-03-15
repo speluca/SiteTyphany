e5487c6633e63cd2e7d7f95217811141ec8caf9b
AiDentoPL <109866266+AiDentoPL@users.noreply.github.com>
2024-03-14 23:17:57 -0300

    teste 3

--- menu.js
+++ menu.js
@@ -1,13 +1,10 @@
-// Seleciona o botão de hambúrguer e o menu de navegação
-var hamburgerBtn = document.querySelector('.hamburger-btn');
-var menuLinks = document.querySelector('.menu-links');
-
-// Adiciona um ouvinte de evento de clique ao botão de hambúrguer
-hamburgerBtn.addEventListener('click', function() {
-    // Alterna a exibição do menu de navegação
-    if (menuLinks.style.display === 'none') {
-        menuLinks.style.display = 'block';
+function toggleMenu() {
+    var menuContent = document.getElementById("menuContent");
+    if (menuContent.style.display === "none" || menuContent.style.display === "") {
+      menuContent.style.display = "block";
     } else {
-        menuLinks.style.display = 'none';
+      menuContent.style.display = "none";
     }
-});
+  }
+  
+  
